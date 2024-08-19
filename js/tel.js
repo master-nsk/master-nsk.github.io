fcbb80c5151fd2b96e35e89bc625f325458055d4
master-nsk <65942028+master-nsk@users.noreply.github.com>
2024-08-19 21:47:07 +0700

    Delete js/tel.js
    Удалил js файл в котором был указан мой номер телефона

--- js/tel.js
+++
@@ -1,8 +0,0 @@
- window.onload = function() {
-
- }
-function tel(){
-	document.getElementById('phone').href="tel:+79137382153";
-	var wbody = document.getElementById('body').offsetWidth;
-	if (wbody<500) yaCounter40866834.reachGoal('mobile-click-phone');
-}
