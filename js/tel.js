73cd1c6369658107d4c18f00735dc4b245b13a6b
master-nsk <65942028+master-nsk@users.noreply.github.com>
2024-12-09 16:23:31 +0700

    Сделал защиту намера телефона в js.tel

--- js/tel.js
+++ js/tel.js
@@ -3,7 +3,7 @@
 
 	function tel(){
 		console.log('click');
-		document.getElementById('phone').href="tel:+79132077303";
+		document.getElementById('phone').href="tel:+7913207730"+9/3;
 		var wbody = document.getElementById('body').offsetWidth;
 		if (wbody<500) ym(45965133,'reachGoal','mobile-click-phone');
 	}
