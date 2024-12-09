92e83d5d06b7fe6c93f44b165719560cdb717856
master-nsk <65942028+master-nsk@users.noreply.github.com>
2024-12-09 16:16:32 +0700

    Изменил js код

--- js/tel.js
+++ js/tel.js
@@ -1,8 +1,15 @@
  window.onload = function() {
 
- 	function tel(){
- 		document.getElementById('phone').href="tel:+79132077303";
- 		var wbody = document.getElementById('body').offsetWidth;
- 		if (wbody<500) yaCounter40866834.reachGoal('mobile-click-phone');
+
+	function tel(){
+		console.log('click');
+		document.getElementById('phone').href="tel:+79132077303";
+		var wbody = document.getElementById('body').offsetWidth;
+		if (wbody<500) ym(45965133,'reachGoal','mobile-click-phone');
+	}
+
+ 	phone.onclick = function() {
+ 		tel();
  	}
+
 }
